fd7fef0c29f84a124c454f9b4fad607549baeb53
Nour Tofi <69059723+NourTofi@users.noreply.github.com>
2022-08-05 23:21:09 +0300

    The root if.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import { BinaryTreeNode, drawBinaryTree } from 'binary-tree-visualizer';
 
 const root = new BinaryTreeNode(100);
 drawBinaryTree(root, document.querySelector('canvas'));
+
+const left = new BinaryTreeNode(23);
+drawBinaryTree(left, document.querySelector('canvas'));
