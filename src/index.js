7f5f4ed39206bcd64db52228b11fea04b89ebace
mohammad1990 <mohammad.tofi@gmail.com>
2022-08-05 22:48:57 +0200

    Change number of tree

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import { BinaryTreeNode, drawBinaryTree } from 'binary-tree-visualizer';
 const root = new BinaryTreeNode(100);
 drawBinaryTree(root, document.querySelector('canvas'));
 
-const left = new BinaryTreeNode(23);
+const left = new BinaryTreeNode(21);
 drawBinaryTree(left, document.querySelector('canvas'));
 
-const right = new BinaryTreeNode(52);
+
+
+const right = new BinaryTreeNode(53);
 drawBinaryTree(right, document.querySelector('canvas'));
+// Change in the file 
